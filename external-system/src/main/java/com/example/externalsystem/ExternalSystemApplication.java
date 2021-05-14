866edf33a08a8b119277bdc09a19ac9ca9934c4d
ikuzmin <Igor.Kuzmin@artezio.com>
2021-05-14 19:35:49 +0300

    New feature +

--- external-system/src/main/java/com/example/externalsystem/ExternalSystemApplication.java
+++ external-system/src/main/java/com/example/externalsystem/ExternalSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExternalSystemApplication {
 	public static void main(String[] args) {
-		System.out.print("Feature");
+		System.out.print("Feature+");
 		SpringApplication.run(ExternalSystemApplication.class, args);
 	}
 
