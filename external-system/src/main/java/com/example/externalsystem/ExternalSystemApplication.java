fb98936aed29f4beaceff2baca5f665bc7b384b9
ikuzmin <Igor.Kuzmin@artezio.com>
2021-05-14 16:56:18 +0300

    Removed error for test purposes.

--- external-system/src/main/java/com/example/externalsystem/ExternalSystemApplication.java
+++ external-system/src/main/java/com/example/externalsystem/ExternalSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExternalSystemApplication {
-	err
 	public static void main(String[] args) {
 		SpringApplication.run(ExternalSystemApplication.class, args);
 	}
